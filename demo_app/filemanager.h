d655328af8c8afa6500394070b579f6e590929f5
Mariia Kuzyshyn <mariia.kuzyshyn.pz.2019@lpnu.ua>
2022-06-14 22:01:52 +0300

    Move functions definitions to the .cpp file

--- demo_app/filemanager.h
+++ demo_app/filemanager.h
@@ -12,50 +12,11 @@ class FileManager
 public:
     FileManager();
 
-    FileTreeElement *generateFileTree(const QString &rootPath)
-    {
-        if (!QFile::exists(rootPath))
-            return {};
-
-        QDir currentDir(rootPath);
-        FileTreeElement *fileTreeRoot = new FileTreeElement(rootPath, getDirectorySize(currentDir.absolutePath()), nullptr);
-        fileTreeRoot->setChildElements(getInnerFiles(QDir(rootPath), fileTreeRoot));
-
-        return fileTreeRoot;
-    }
-
-    quint64 getDirectorySize(const QString &directory)
-    {
-        quint64 size = 0;
-        QDirIterator iterator(directory, QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
-        while (iterator.hasNext()) {
-            iterator.next();
-            size += iterator.fileInfo().size();
-        }
-        return size;
-    }
+    FileTreeElement *generateFileTree(const QString &rootPath);
+    quint64 getDirectorySize(const QString &directory);
 
 private:
-    QList<FileTreeElement *> getInnerFiles(const QDir &currenDir, FileTreeElement *parent)
-    {
-        QList<FileTreeElement *> innerFiles;
-
-        for (auto &fileElement : currenDir.entryInfoList(QDir::NoDot | QDir::NoDotDot | QDir::AllEntries))
-        {
-             FileTreeElement *fileTreeElement = new FileTreeElement(fileElement.fileName(), fileElement.size(), parent);
-
-             if (fileElement.isDir())
-             {
-                 QList<FileTreeElement *> innerFiles = getInnerFiles(QDir(fileElement.absoluteFilePath()), fileTreeElement);
-                 fileTreeElement->setChildElements(innerFiles);
-                 fileTreeElement->setFileSize(getDirectorySize(fileElement.absoluteFilePath()));
-             }
-
-             innerFiles.append(fileTreeElement);
-        }
-
-        return innerFiles;
-    }
+    QList<FileTreeElement *> getInnerFiles(const QDir &currenDir, FileTreeElement *parent);
 };
 
 #endif // FILEMANAGER_H
