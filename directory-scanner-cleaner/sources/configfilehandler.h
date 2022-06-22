5484494d6427d59327e522b7df8923fd081a3c9e
Mariia Kuzyshyn <mariia.kuzyshyn.pz.2019@lpnu.ua>
2022-06-22 17:19:18 +0300

    Add classes for handling the config file

---
+++ directory-scanner-cleaner/sources/configfilehandler.h
@@ -0,0 +1,30 @@
+#ifndef CONFIGFILEHANDLER_H
+#define CONFIGFILEHANDLER_H
+
+#include <QString>
+#include <QSettings>
+#include <QCoreApplication>
+
+class ConfigFileHandler
+{
+public:
+    ConfigFileHandler(const QString &organization = "Some Organization",
+                      const QString &application = "Directory Scanner & Cleaner",
+                      QSettings::Scope scope = QSettings::SystemScope,
+                      QSettings::Format format = QSettings::IniFormat);
+
+    QString getDeletionFilePath() const;
+    void setDeletionFilePath(QString filePath);
+    void readSettings();
+    void writeSettings();
+
+private:
+QString m_Organization;
+QString m_Application;
+QSettings::Scope m_Scope;
+QSettings::Format m_Format;
+
+QString m_DeletionFilePath;
+};
+
+#endif // CONFIGFILEHANDLER_H
